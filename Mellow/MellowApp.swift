2587e2dbafa15237107d4b0199560b25ea4fecff
Julien MacBook Pro <julien.f2004@icloud.com>
2026-06-09 21:57:56 +0200

    Initial Commit

---
+++ Mellow/MellowApp.swift
@@ -0,0 +1,17 @@
+//
+//  MellowApp.swift
+//  Mellow
+//
+//  Created by Julien Fernandes on 09/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct MellowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
